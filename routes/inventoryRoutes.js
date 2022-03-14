7bb8a85c63431ec0961d6519c76a9c696e02c999
Valentin Manaila <valentin.manaila@gmail.com>
2022-03-14 19:25:15 +0000

    get all warehouses list complete

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,27 @@
+//(/inventory)
+
+//! SORT WILL HAPPEN on GET route using params e.g.
+// /inventory ? sort_cat=category & sort=asc
+//GET("/")
+// TaskBack-End: API to GET List of all Inventory Items
+// J2W-21Medium-
+
+//GET("/:iId")
+// TaskBack-End: API to GET a Single Inventory Item
+// J2W-22Medium-
+
+//GET("/warehouse/:wId")
+// TaskBack-End: API to GET Inventories for a Given Warehouse
+// J2W-23Medium-
+
+//POST("/")
+// TaskBack-End: API to POST/CREATE a New Inventory Item
+// J2W-24Medium-
+
+//POST("/:iId")
+// TaskBack-End: API to PUT/PATCH/EDIT an Inventory Item
+// J2W-25Medium
+
+//DELETE("/:iId")
+// TaskBack-End: API to DELETE an Inventory Item
+// J2W-26
--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -3,7 +3,29 @@ const router = require("express").Router();
 const warehouseController = require("../controllers/warehouseController");
 
 
-router.get("/",warehouseController.getWarehousesList);
+//! SORT WILL HAPPEN on GET route using params e.g.
+// /warehouse ? sort_cat=category & sort=asc
 
+//GET("/")
+// TaskBack-End: API to GET List of All Warehouses
+// J2W-10Medium-
+router.get("/", warehouseController.getWarehousesList);
+
+
+//GET("/:wId")
+// TaskBack-End: API to GET a Single Warehouse
+// J2W-11Medium-
+
+//POST("/")
+// TaskBack-End: API to POST/CREATE a New Warehouse
+// J2W-12Medium-
+
+//PATCH("/:wId")
+// TaskBack-End: API to PUT/PATCH/EDIT a Warehouse
+// J2W-14Medium-
+
+//DELETE("/:wId")
+// TaskBack-End: API to DELETE a Warehouse
+// J2W-15Medium
 
 module.exports = router;
